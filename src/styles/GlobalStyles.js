a47fa6f785ba72554cb9f8c1c5dcdadb7ed25739
Kenntomi <marci.kenntomi@gmail.com>
2025-09-07 14:36:46 -0500

    Corregir importación de fuentes en GlobalStyles.js

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
+// Las fuentes ya están importadas en el index.html, no es necesario importarlas aquí
 const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap');
 
   * {
     margin: 0;
